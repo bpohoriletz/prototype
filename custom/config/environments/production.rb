61f9615079f74519eb9c60a3fd32e8ba4d14fd90
Bohdan Pohorilets <bohdan.pohorilets@gmail.com>
2024-06-18 12:15:53 +0300

    Don't care if mail not sent

--- custom/config/environments/production.rb
+++ custom/config/environments/production.rb
@@ -5,4 +5,5 @@ Rails.application.configure do
   #                                                                          #
   ############################################################################
   config.action_mailer.default_url_options = { host: :localhost }
+  config.action_mailer.raise_delivery_errors = false
 end
