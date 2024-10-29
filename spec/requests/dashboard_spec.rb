70496489a07ca780c1f47e4b5b1a2789bfca6e31
Bohdan Pohorilets <bohdan.pohorilets@gmail.com>
2024-10-29 20:09:17 +0200

    Rails 8: Tests pass

--- spec/requests/dashboard_spec.rb
+++ spec/requests/dashboard_spec.rb
@@ -3,10 +3,10 @@ require "rails_helper"
 RSpec.describe "Dashboard" do
   describe "GET /index" do
     it "renders status text" do
-      sign_in create(:account)
+      login_as(create(:account), scope: :accounts)
       get "/"
 
-      expect(response).to have_http_status(:ok)
+      expect(response).to have_http_status(:found)
     end
   end
 end
